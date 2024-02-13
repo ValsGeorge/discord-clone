b5d5602c87690a720dedf3977cfd56a3460e5c1b
VALS George <56400505+ValsGeorge@users.noreply.github.com>
2024-02-13 10:33:41 +0200

    improve even more

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,14 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-    test_variable = '';
-    constructor(private httpClient: HttpClient) {}
-    ngOnInit(): void {}
-}
+export class HomeComponent {}
--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+    isSidePanelOpen = false;
+
+    toggleSidePanel() {
+        this.isSidePanelOpen = !this.isSidePanelOpen;
+    }
+
+    @ViewChild('sidePanel') sidePanel: ElementRef | undefined;
+
+    @HostListener('document:click', ['$event'])
+    closeSidePanel(event: Event): void {
+        const target = event.target as HTMLElement;
+        if (this.sidePanel) {
+            const isInside = this.sidePanel.nativeElement.contains(target);
+
+            if (
+                this.isSidePanelOpen &&
+                !isInside &&
+                !target.closest('#menuToggle')
+            ) {
+                this.isSidePanelOpen = false;
+            }
+        }
+    }
+}
